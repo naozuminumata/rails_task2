344c564e86206bef798c299e3ee71f73e797d6df
Naozumi Numata <naozumi225@gmail.com>
2020-05-03 22:00:08 +0900

    Add CRUD function to CRUD_app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+ class UsersController < ApplicationController
+    # 一覧画面に対応するアクション
+    def index
+        @users = User.all
+    end
+
+    #新規作成画面に対応するアクション
+    def new
+        @user = User.new
+    end
+
+    #新規登録するためのアクション
+    def create
+        User.create(user_params)
+        redirect_to action: :index
+    end
+
+    #編集画面に対応するアクション
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    #更新を行うためのアクション
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        redirect_to action: :index
+    end
+
+    #削除をするためのアクション
+    def destroy
+        user = User.find(params[:id])
+        user.delete
+        redirect_to action: :index
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name,:age)
+    end
+end
